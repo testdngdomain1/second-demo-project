e2e58abae1b42ad53e82ec9b0fcfbe76fcc2753b
testdngdomain1 <85888376+testdngdomain1@users.noreply.github.com>
2022-05-21 20:48:43 +0530

    Update changeTag.sh

--- changeTag.sh
+++ changeTag.sh
@@ -1,1 +1,1 @@
-sed 's/TAG/${env.BUILD_NUMBER}/g' /opt/Docker/workspace/Deploy_To_Tomcat_Container_Using_Ansible_Pipeline_Project/play.yml
+sed "s/TAG/$1/g" /opt/Docker/workspace/Deploy_To_Tomcat_Container_Using_Ansible_Pipeline_Project/play.yml
